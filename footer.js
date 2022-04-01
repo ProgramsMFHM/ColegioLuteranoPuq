622747088aa0ac6c82da051b50c679d495f5a353
Milton Fabricio Hernández Morales <milton.fabricio.hernandez.morales@gmail.com>
2022-04-01 18:07:52 -0300

    Cambiando los Headers y Footers por Js

---
+++ footer.js
@@ -0,0 +1,45 @@
+// Including the footer
+
+body = document.querySelector("body");
+
+let temporalFooter = document.createElement("FOOTER");
+
+temporalFooter.classList.add("footer")
+
+temporalFooter.innerHTML =`
+    <div class="socialMedia">
+            
+        <div class="socialMedia__network">
+            <a href="https://www.facebook.com/Colegio-Luterano-de-Punta-Arenas-110910395603597/" target="0_blank">
+                <i class="fab fa-facebook"></i>
+                <p>Facebook</p>
+            </a>
+        </div>
+
+        <div class="socialMedia__network">
+            <a href="https://goo.gl/maps/Y8bUwNEFcvtpjchEA" target="0_blank">
+                <i class="fas fa-map"></i>
+                <p>Ubicación</p>
+            </a>
+        </div>
+
+        </div>
+
+        <div class="schoolMotto">
+        <p>"La Educación de los hijos es un servicio a y para Dios."</p>
+        <div class="shields">
+
+            <a href="https://www.facebook.com/Iglesia-Evang%C3%A9lica-Luterana-en-Magallanes-129087137102459" target="0_blank">
+                <img src="/sources/Images/shields/Escudo-IELMA.webp" alt="Escudo Iglesia Evangélica Luterana en Magallanes">
+            </a>
+
+            <a href="https://ielch.cl/" target="0_blank">
+                <img src="/sources/Images/shields/Escudo-IELCH.webp" alt="Escudo Iglesia Evangélica Luterana en Chile">
+            </a>
+
+        </div>
+
+    </div>
+`;
+
+body.appendChild(temporalFooter)
---
+++ header.js
@@ -0,0 +1,71 @@
+// Including the Header
+
+body = document.querySelector("body");
+
+let temporalHeader = document.createElement("HEADER");
+
+temporalHeader.classList.add("header")
+
+temporalHeader.innerHTML =`
+    <a  href="/index.html" class="heading">
+        <img src="/sources/Images/shields/Escudo_colegio.webp" alt="Escudo colegio luterano de Punta Arenas" class="heading__image">
+        <div class="heading__text">
+            <h1 class="heading__text-1">Colegio Luterano</h1>
+            <p class="heading__text-2">Punta Arenas</p>
+        </div>
+    </a>
+
+    <div class="menuBtn">
+
+        <i class="fas fa-bars"></i>
+
+    </div>
+
+    <nav class="menuContainer">
+        <ul class="menu">
+            <li class="menu__li"><a href="/index.html"><i class="fa-solid fa-house"></i> Inicio</a></li>
+            <li class="menu__li containerSubMenu">
+                <a href="#" class="subMenuBtn">¿Quienes somos?<i class="fa-solid fa-angle-down subMenuBtn__icon"></i></a>
+                <ul class="subMenu">
+                    <li class="subMenu__li"><a href="/misionYVision/misionYVision.html">Misión y Visión</a></li>
+                    <li class="subMenu__li"><a href="#">Historia</a></li>
+                    <li class="subMenu__li"><a href="#">Iglesia Evangélica Luterana</a></li>
+                    <li class="subMenu__li"><a href="#">Información</a></li>
+                    <li class="subMenu__li"><a href="/documents/pdf/PEI-colegio-luterano.pdf" target="0_blank">Proyecto Educativo Institucional</a></li>
+                </ul>
+            </li>
+            <li class="menu__li"><a href="/funcionarios/funcionarios.html">Equipo de trabajo</a></li>
+            <li class="menu__li"><a href="/documents/documents.html">Documentos</a></li>
+            <li class="menu__li"><a href="/news/news.html">Noticias</a></li>
+            <li class="menu__li"><a href="#">Contacto</a></li>
+        </ul>
+    </nav>
+`;
+
+body.appendChild(temporalHeader);
+
+// Creating the responsive menu function
+
+const menuButton = document.querySelector(".menuBtn");
+const menu = document.querySelector(".menuContainer");
+const subMenuButtons = document.querySelectorAll(".subMenuBtn");
+
+menuButton.addEventListener("click", () =>{menu.classList.toggle("showMenu");})
+
+for (let i = 0; i < subMenuButtons.length; i++) {
+    subMenuButtons[i].addEventListener("click",()=>{
+        if(window.innerWidth < 1577){
+            const subMenu = subMenuButtons[i].nextElementSibling;
+            const height = subMenu.scrollHeight;
+
+            if(subMenu.classList.contains("desplegar")){
+                subMenu.classList.remove("desplegar");
+                subMenu.removeAttribute("style");
+            }
+            else{
+                subMenu.classList.add("desplegar");
+                subMenu.style.height = height+"px";
+            }
+        }            
+    })
+}
